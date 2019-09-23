1ab5a64175d6aed425182935f8b079e75dac39fd
Askar Karasaev <askar.karasaev@gmail.com>
2019-09-23 21:19:36 +0900

    section 4 lesson 8 updated

--- tests/Feature/ContactsTest.php
+++ tests/Feature/ContactsTest.php
@@ -13,6 +13,7 @@ class ContactsTest extends TestCase
     /** @test */
     public function a_contact_can_be_added()
     {
+        $this->withoutExceptionHandling();
         $this->post('/api/contacts',['name' => 'Test Name']);
 
         $this->assertCount(1,Contact::all());
