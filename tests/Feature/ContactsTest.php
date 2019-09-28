6834683fce5a3962aafc1e990e67dfaa07ec4791
Askar Karasaev <askar.karasaev@gmail.com>
2019-09-28 20:28:00 +0900

    Deleting a Contact

--- tests/Feature/ContactsTest.php
+++ tests/Feature/ContactsTest.php
@@ -97,6 +97,16 @@ class ContactsTest extends TestCase
         $this->assertEquals('ABC String', $contact->company);
     }
 
+    /** @test */
+    public function a_contact_can_be_deleted()
+    {
+        $contact = factory(Contact::class)->create();
+
+        $response = $this->delete('/api/contacts/' . $contact->id, $this->data());
+
+        $this->assertCount(0, Contact::all());
+    }
+
     private function data()
     {
         return [
