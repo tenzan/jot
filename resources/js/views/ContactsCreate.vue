331041fae35b704a8c093f98e9eea4b7ec3c2c9b
Askar Karasaev <askar.karasaev@gmail.com>
2019-10-14 12:59:53 +0900

    Reusable Input Vue Component

--- resources/js/views/ContactsCreate.vue
+++ resources/js/views/ContactsCreate.vue
@@ -1,25 +1,45 @@
 <template>
-    <dev>
+    <div>
         <form>
-            <div class="relative pb-4">
-                <label for="name" class="text-blue-500 pt-2 uppercase text-xs font-bold absolute">Contact Name</label>
-                <input id="name" type="text"
-                       class="pt-8 w-full text-gray-900 border-b pb-2 focus:outline-none focus:border-blue-400"
-                       placeholder="Contact Name">
-            </div>
+            <InputField name="name" label="Contact Name"
+                        placeholder="Contact Name" @update:field="form.name = $event"/>
+            <InputField name="email" label="Contact Email"
+                        placeholder="Contact Email" @update:field="form.email=$event"/>
+            <InputField name="company" label="Company"
+                        placeholder="Company" @update:field="form.company = $event"/>
+            <InputField name="birthday" label="Birthday"
+                        placeholder="MM/DD/YYYY" @update:field="form.birthday = $event"/>
 
             <div class="flex justify-end">
                 <button class="py-2 px-4 rounded text-red-700 border mr-5 hover:border-red-700">Cancel</button>
                 <button class="bg-blue-500 py-2 px-4 rounded text-white hover:bg-blue-400">Add New Contact</button>
             </div>
         </form>
-    </dev>
+    </div>
 
 </template>
 
 <script>
+    import InputField from "../components/InputField";
+
     export default {
-        name: "ContactsCreate"
+        name: "ContactsCreate",
+
+        components: {
+            InputField
+        },
+
+        data: function () {
+            return {
+                form: {
+                    'name': '',
+                    'email': '',
+                    'company': '',
+                    'birthday': '',
+                }
+            }
+
+        }
     }
 </script>
 
