d77d8042b543aab0e2b931ef660d0c46379b0b1f
Askar Karasaev <askar.karasaev@gmail.com>
2019-10-20 00:10:37 +0900

    Show View Design

--- resources/js/views/ContactsShow.vue
+++ resources/js/views/ContactsShow.vue
@@ -1,6 +1,31 @@
 <template>
     <div>
+        <div class="flex justify-between">
+            <div class="text-blue-400">
+                < Back
+            </div>
+            <div>
+                <router-link :to="'/contact/' + contact.id + '/edit/'"
+                             class="px-4 py-2 rounded text-green-500 border border-green-500 text-sm font-bold mr-2">
+                    Edit
+                </router-link>
+                <a href="#" class="px-4 py-2 rounded text-red-500 border border-red-500 text-sm font-bold">Delete</a>
+            </div>
+        </div>
 
+        <div class="flex items-center pt-6">
+            <div>VG</div>
+            <p class="pl-5 text-xl">{{ contact.name }}</p>
+        </div>
+
+        <p class="pt-6 text-gray-600 font-bold uppercase text-sm">Contact</p>
+        <p class="pt-2 text-blue-400">{{ contact.email }}</p>
+
+        <p class="pt-6 text-gray-600 font-bold uppercase text-sm">Company</p>
+        <p class="pt-2 text-blue-400">{{ contact.company }}</p>
+
+        <p class="pt-6 text-gray-600 font-bold uppercase text-sm">Birthday</p>
+        <p class="pt-2 text-blue-400">{{ contact.birthday }}</p>
     </div>
 </template>
 
