abc6bc8f64f06309dfe793c7b4b83a827df7166d
Askar Karasaev <askar.karasaev@gmail.com>
2019-10-20 20:20:43 +0900

    Searching Feature - Part 1

---
+++ app/Http/Controllers/SearchController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Contact;
+use App\Http\Resources\Contact as ContactResource;
+use Illuminate\Http\Request;
+
+class SearchController extends Controller
+{
+    public function index()
+    {
+        $data = request()->validate()([
+            'searchTerm' => 'required',
+        ]);
+
+        $contacts = Contact::search($data['searchItem'])->get();
+
+        return ContactResource::collection($contacts);
+    }
+
+}
